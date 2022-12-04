6fbf7a85eb98cdde5c1cf12f95f5d96136b6d8f7
Eric Ung <ungericw@tutanota.com>
2022-12-04 09:16:52 -0600

    problem 4

---
+++ AdventOfCode2022.Tests/Problem04Tests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestClass]
+    public class Problem04Tests
+    {
+        [TestMethod]
+        public void OverlappingPairsTest()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem04input.txt");
+            
+            List<IList<int>> list = new List<IList<int>>();
+
+            foreach (string s in strings)
+            {
+                if (s != String.Empty)
+                {
+                    string[] pairs = s.Split(',');
+                    List<int> newList = new List<int>();
+                    foreach (string pair in pairs)
+                    {
+                        string[] pairNum = pair.Split('-');
+                        newList.Add(Convert.ToInt32(pairNum[0]));
+                        newList.Add(Convert.ToInt32(pairNum[1]));
+                    }
+                    list.Add(newList);
+                }
+            }
+
+            Problem04 problem04= new Problem04();
+            #endregion
+
+            #region Act
+            var score = problem04.OverlappingPairs(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(score, 542);
+            #endregion
+
+        }
+
+        [TestMethod]
+        public void OverlappingPairs2Test()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem04input.txt");
+            
+            List<IList<int>> list = new List<IList<int>>();
+
+            foreach (string s in strings)
+            {
+                if (s != String.Empty)
+                {
+                    string[] pairs = s.Split(',');
+                    List<int> newList = new List<int>();
+                    foreach (string pair in pairs)
+                    {
+                        string[] pairNum = pair.Split('-');
+                        newList.Add(Convert.ToInt32(pairNum[0]));
+                        newList.Add(Convert.ToInt32(pairNum[1]));
+                    }
+                    list.Add(newList);
+                }
+            }
+
+            Problem04 problem04= new Problem04();
+            #endregion
+
+            #region Act
+            var score = problem04.OverlappingPairs2(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(score, 900);
+            #endregion
+
+        }
+    }
+}
---
+++ AdventOfCode2022/Problem04.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Problem04
+    {
+        public int OverlappingPairs(IList<IList<int>> list)
+        {
+            int pairs = 0;
+
+            foreach (IList<int> pair in list)
+            {
+                if ((pair[2] >= pair[0] && pair[3] <= pair[1])||(pair[0] >= pair[2] && pair[1] <= pair[3]))
+
+                {
+                    pairs++;
+                }
+            }
+
+            return pairs;
+        }
+        public int OverlappingPairs2(IList<IList<int>> list)
+        {
+            int pairs = 0;
+
+            foreach (IList<int> pair in list)
+            {
+                if ((pair[2] >= pair[0] && pair[3] <= pair[1])||(pair[0] >= pair[2] && pair[1] <= pair[3])||
+                    (pair[2] <= pair[0] && pair[0] <= pair[3])||
+                    (pair[0] <= pair[2] && pair[2] <= pair[1]))
+                {
+                    pairs++;
+                }
+            }
+
+            return pairs;
+        }
+    }
+}
