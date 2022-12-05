f1178f9d7ca6e1832cc9d56dff4372e1144755bc
Eric Ung <ungericw@tutanota.com>
2022-12-05 09:34:17 -0600

    Problem 05

---
+++ AdventOfCode2022.Tests/Problem05Tests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestClass]
+    public class Problem05Tests
+    {
+        [TestMethod]
+        public void MoveCratesSuccess()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem05input.txt");
+            
+            List<Stack<char>> stacks = new List<Stack<char>>();
+            int i = 0;
+            
+            while (strings[i] != String.Empty)
+            {
+                i++;
+            }
+
+            string[] stackStr = strings[i - 1].Split(" ");
+
+            foreach(string s in stackStr)
+            {
+                if (s != string.Empty)
+                {
+                    stacks.Add(new Stack<char>());
+                }
+            }
+
+            for (int k = i - 2; k >= 0; k--)
+            {
+                for (int j = 1, s = 0; j < strings[k].Length; j+=4, s++)
+                {
+                    if (strings[k][j] != ' ')
+                    {
+                        stacks[s].Push(strings[k][j]);
+                    }
+                }
+            }
+
+            i++;
+            List<IList<int>> moves = new List<IList<int>>();
+            for (int k = i; k < strings.Length; k++)
+            {
+                if (strings[k] == String.Empty)
+                {
+                    break;
+                }
+
+                List<int> move = new List<int>();
+                var nums = strings[k].Split(" ");
+                move.Add(Convert.ToInt32(nums[1]));
+                move.Add(Convert.ToInt32(nums[3]));
+                move.Add(Convert.ToInt32(nums[5]));
+                moves.Add(move);
+            }
+            #endregion
+
+            #region Act
+            var results = Problem05.MoveCrates(stacks, moves);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(results, new string("VRWBSFZWM"));
+            #endregion
+        }
+
+        [TestMethod]
+        public void MoveCrates9001Success()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem05input.txt");
+            
+            List<Stack<char>> stacks = new List<Stack<char>>();
+            int i = 0;
+            
+            while (strings[i] != String.Empty)
+            {
+                i++;
+            }
+
+            string[] stackStr = strings[i - 1].Split(" ");
+
+            foreach(string s in stackStr)
+            {
+                if (s != string.Empty)
+                {
+                    stacks.Add(new Stack<char>());
+                }
+            }
+
+            for (int k = i - 2; k >= 0; k--)
+            {
+                for (int j = 1, s = 0; j < strings[k].Length; j+=4, s++)
+                {
+                    if (strings[k][j] != ' ')
+                    {
+                        stacks[s].Push(strings[k][j]);
+                    }
+                }
+            }
+
+            i++;
+            List<IList<int>> moves = new List<IList<int>>();
+            for (int k = i; k < strings.Length; k++)
+            {
+                if (strings[k] == String.Empty)
+                {
+                    break;
+                }
+
+                List<int> move = new List<int>();
+                var nums = strings[k].Split(" ");
+                move.Add(Convert.ToInt32(nums[1]));
+                move.Add(Convert.ToInt32(nums[3]));
+                move.Add(Convert.ToInt32(nums[5]));
+                moves.Add(move);
+            }
+            #endregion
+
+            #region Act
+            var results = Problem05.MoveCrates9001(stacks, moves);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(results, new string("RBTWJWMCF"));
+            #endregion
+        }
+
+    }
+}
---
+++ AdventOfCode2022/Problem05.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Problem05
+    {
+        public static string MoveCrates(IList<Stack<char>> crates, IList<IList<int>> moves)
+        {
+            List<char> list = new List<char>();
+
+            foreach(var move in moves)
+            {
+                int m = move[0];
+                for(int i = 0; i < m; i++)
+                {
+                    var to = move[2] - 1;
+                    var from = move[1] - 1;
+                    crates[to].Push(crates[from].Pop());
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < crates.Count; i++)
+            {
+                sb.Append(crates[i].Peek());
+            }
+
+            return sb.ToString();
+        }
+
+        public static string MoveCrates9001(IList<Stack<char>> crates, IList<IList<int>> moves)
+        {
+            List<char> list = new List<char>();
+
+            foreach(var move in moves)
+            {
+                int m = move[0];
+                List<char> order = new List<char>();
+                var to = move[2] - 1;
+                var from = move[1] - 1;
+
+                for(int i = 0; i < m; i++)
+                {
+                    order.Add(crates[from].Pop());
+                }
+
+                for (int i = order.Count-1; i >= 0; i--)
+                {
+                    crates[to].Push(order[i]);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < crates.Count; i++)
+            {
+                sb.Append(crates[i].Peek());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
