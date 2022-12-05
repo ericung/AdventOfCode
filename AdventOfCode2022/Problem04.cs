f369014121ecc99e688573db688d31f9aa30b6dc
Eric Ung <ungericw@tutanota.com>
2022-12-05 08:50:29 -0600

    Cleaning up

--- AdventOfCode2022/Problem04.cs
+++ AdventOfCode2022/Problem04.cs
@@ -30,8 +30,7 @@ namespace AdventOfCode2022
             foreach (IList<int> pair in list)
             {
                 if ((pair[2] >= pair[0] && pair[3] <= pair[1])||(pair[0] >= pair[2] && pair[1] <= pair[3])||
-                    (pair[2] <= pair[0] && pair[0] <= pair[3])||
-                    (pair[0] <= pair[2] && pair[2] <= pair[1]))
+                    (pair[2] <= pair[0] && pair[0] <= pair[3])||(pair[0] <= pair[2] && pair[2] <= pair[1]))
                 {
                     pairs++;
                 }
