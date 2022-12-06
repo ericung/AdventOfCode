f1777c888c8d3a47b63ead3f1d999af0d6aa1045
Eric Ung <ungericw@tutanota.com>
2022-12-06 07:37:39 -0600

    Remove unused usings

--- AdventOfCode2022.Tests/Problem05Tests.cs
+++ AdventOfCode2022.Tests/Problem05Tests.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2022.Tests
+﻿namespace AdventOfCode2022.Tests
 {
     [TestClass]
     public class Problem05Tests
