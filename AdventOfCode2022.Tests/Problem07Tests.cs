bcee70affe729ad680a4517ee0448f58d1003bd9
Eric Ung <ungericw@tutanota.com>
2022-12-07 09:35:31 -0600

    Problem 07

---
+++ AdventOfCode2022.Tests/Problem07Tests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestClass]
+    public class Problem07Tests
+    {
+        [TestMethod]
+        public void FindSumTest1Success()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem07test1.txt");
+            List<string[]> list = new List<string[]>();
+
+            foreach(string s in strings)
+            {
+                list.Add(s.Split(' '));
+            }
+            Problem07 problem = new Problem07();
+            #endregion
+
+            #region Act
+            long sum = problem.FindSum(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(95437, sum);
+            #endregion
+        }
+
+        [TestMethod]
+        public void FindSumInputSuccess()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem07input.txt");
+            List<string[]> list = new List<string[]>();
+
+            foreach(string s in strings)
+            {
+                list.Add(s.Split(' '));
+            }
+            Problem07 problem = new Problem07();
+            #endregion
+
+            #region Act
+            long sum = problem.FindSum(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(1297683, sum);
+            #endregion
+        }
+
+        [TestMethod]
+        public void FindDirectoryTest1Success()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem07test1.txt");
+            List<string[]> list = new List<string[]>();
+
+            foreach(string s in strings)
+            {
+                list.Add(s.Split(' '));
+            }
+            Problem07 problem = new Problem07();
+            #endregion
+
+            #region Act
+            long sum = problem.FindDirectory(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(24933642, sum);
+            #endregion
+        }
+
+        [TestMethod]
+        public void FindDirectoryInputSuccess()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem07input.txt");
+            List<string[]> list = new List<string[]>();
+
+            foreach(string s in strings)
+            {
+                list.Add(s.Split(' '));
+            }
+            Problem07 problem = new Problem07();
+            #endregion
+
+            #region Act
+            long sum = problem.FindDirectory(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(5756764, sum);
+            #endregion
+        }
+
+    }
+}
---
+++ AdventOfCode2022/Problem07.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Problem07
+    {
+        public long FindSum(List<string[]> terminal)
+        {
+            Dictionary<string, List<string[]>> directory = new Dictionary<string, List<string[]>>();
+            string curPath = "/";
+            for (int i = 0; i < terminal.Count; i++)
+            {
+                var line = terminal[i];
+                if (line[0] == "$")
+                {
+                    switch(line[1])
+                    {
+                        case "cd": 
+                            switch (line[2])
+                            {
+                                case "..":
+                                    {
+                                        var split = curPath.Split('/');
+                                        StringBuilder sb = new StringBuilder();
+                                        for (int j = 0; j < split.Length-1; j++)
+                                        {
+                                            sb.Append(split[j]);
+                                            if (j < split.Length - 2)
+                                            {
+                                                sb.Append("/");
+                                            }
+                                        }
+                                        curPath = sb.ToString();
+                                        break;
+                                    }
+                                case "/":
+                                    curPath = "/";
+                                    break;
+                                default:
+                                    {
+                                        if (curPath == "/")
+                                        {
+                                            curPath += line[2];
+                                        }
+                                        else
+                                        {
+                                            curPath += "/" + line[2];
+                                        }
+                                        break;
+                                    }
+                            }
+                            break;
+                        case "ls":
+                            {
+                                for(i = i+1; i < terminal.Count && terminal[i][0] != "$"; i++)
+                                {
+                                    if (!directory.ContainsKey(curPath))
+                                    {
+                                        directory.Add(curPath, new List<string[]>()); 
+                                    }
+                                    if (terminal[i][0] == "dir")
+                                    {
+                                        directory[curPath].Add(new string[] { "dir", (curPath == "/" ? "" : curPath) + "/" + terminal[i][1] });
+                                    }
+                                    else
+                                    {
+                                        directory[curPath].Add(terminal[i]);
+                                    }
+                                }
+                                if (i < terminal.Count && terminal[i][0] == "$")
+                                {
+                                    i--;
+                                }
+                                break;
+                            }
+                    }
+                }
+            }
+
+            Dictionary<string, long> pathSize = Recurse("/", directory, new Dictionary<string, long>());
+
+            long sum = 0;
+
+            foreach (KeyValuePair<string, long> pair in pathSize)
+            {
+                if (pair.Value <= 100000)
+                {
+                    sum += pair.Value;
+                }
+            }
+
+            return sum;
+        }
+
+        public long FindDirectory(List<string[]> terminal)
+        {
+            Dictionary<string, List<string[]>> directory = new Dictionary<string, List<string[]>>();
+            string curPath = "/";
+            for (int i = 0; i < terminal.Count; i++)
+            {
+                var line = terminal[i];
+                if (line[0] == "$")
+                {
+                    switch(line[1])
+                    {
+                        case "cd": 
+                            switch (line[2])
+                            {
+                                case "..":
+                                    {
+                                        var split = curPath.Split('/');
+                                        StringBuilder sb = new StringBuilder();
+                                        for (int j = 0; j < split.Length-1; j++)
+                                        {
+                                            sb.Append(split[j]);
+                                            if (j < split.Length - 2)
+                                            {
+                                                sb.Append("/");
+                                            }
+                                        }
+                                        curPath = sb.ToString();
+                                        break;
+                                    }
+                                case "/":
+                                    curPath = "/";
+                                    break;
+                                default:
+                                    {
+                                        if (curPath == "/")
+                                        {
+                                            curPath += line[2];
+                                        }
+                                        else
+                                        {
+                                            curPath += "/" + line[2];
+                                        }
+                                        break;
+                                    }
+                            }
+                            break;
+                        case "ls":
+                            {
+                                for(i = i+1; i < terminal.Count && terminal[i][0] != "$"; i++)
+                                {
+                                    if (!directory.ContainsKey(curPath))
+                                    {
+                                        directory.Add(curPath, new List<string[]>()); 
+                                    }
+                                    if (terminal[i][0] == "dir")
+                                    {
+                                        directory[curPath].Add(new string[] { "dir", (curPath == "/" ? "" : curPath) + "/" + terminal[i][1] });
+                                    }
+                                    else
+                                    {
+                                        directory[curPath].Add(terminal[i]);
+                                    }
+                                }
+                                if (i < terminal.Count && terminal[i][0] == "$")
+                                {
+                                    i--;
+                                }
+                                break;
+                            }
+                    }
+                }
+            }
+
+            Dictionary<string, long> pathSize = Recurse("/", directory, new Dictionary<string, long>());
+
+            long dirSize = long.MaxValue;
+            long usedSpace = 70000000 - pathSize["/"];
+
+            foreach (KeyValuePair<string, long> pair in pathSize)
+            {
+                if ((pair.Key != "/") &&(usedSpace + pair.Value >= 30000000))
+                {
+                    dirSize = Math.Min(pair.Value, dirSize);
+                }
+            }
+
+            return dirSize;
+        }
+
+        private Dictionary<string,long> Recurse(string curPath, Dictionary<string, List<string[]>> directory, Dictionary<string, long> pathSize)
+        {
+            long sum = 0;
+
+            foreach (var item in directory[curPath])
+            {
+                if (item.Length != 1)
+                {
+                    if (item[0] == "dir")
+                    {
+                        sum += Recurse(item[1], directory, pathSize)[item[1]];
+                    }
+                    else
+                    {
+                        sum += Convert.ToInt32(item[0]);
+                    }
+                }
+            }
+
+            pathSize.Add(curPath, sum);
+            return pathSize;
+        }
+    }
+}
