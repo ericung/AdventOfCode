f55f75b32f44b888ac24583b7b06f45eff051c1c
Eric Ung <ungericw@tutanota.com>
2022-12-10 09:01:37 -0600

    Problem 10

---
+++ AdventOfCode2022.Tests/Problem10Tests.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestClass]
+    public class Problem10Tests
+    {
+        [TestMethod]
+        public void SignalStrengthTest1()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem10test1.txt");
+            List<string[]> list = new List<string[]>();
+
+            foreach (string s in strings)
+            {
+                if (s != string.Empty)
+                {
+                    string[] line = s.Split(' ');
+                    list.Add(line);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Problem10 problem10 = new Problem10();
+            #endregion
+
+            #region Act
+            var strength = problem10.SignalStrength(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(-720, strength);
+            #endregion
+        }
+
+        [TestMethod]
+        public void SignalStrengthTest2()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem10test2.txt");
+            List<string[]> list = new List<string[]>();
+
+            foreach (string s in strings)
+            {
+                if (s != string.Empty)
+                {
+                    string[] line = s.Split(' ');
+                    list.Add(line);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Problem10 problem10 = new Problem10();
+            #endregion
+
+            #region Act
+            var strength = problem10.SignalStrength(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(13140, strength);
+            #endregion
+        }
+
+        [TestMethod]
+        public void SignalStrengthInput()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem10input.txt");
+            List<string[]> list = new List<string[]>();
+
+            foreach (string s in strings)
+            {
+                if (s != string.Empty)
+                {
+                    string[] line = s.Split(' ');
+                    list.Add(line);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Problem10 problem10 = new Problem10();
+            #endregion
+
+            #region Act
+            var strength = problem10.SignalStrength(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(11720, strength);
+            #endregion
+        }
+
+        [TestMethod]
+        public void RenderTest2()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem10test2.txt");
+            List<string[]> list = new List<string[]>();
+
+            foreach (string s in strings)
+            {
+                if (s != string.Empty)
+                {
+                    string[] line = s.Split(' ');
+                    list.Add(line);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Problem10 problem10 = new Problem10();
+            #endregion
+
+            #region Act
+            var screen = problem10.Render(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual("##..##..##..##..##..##..##..##..##..##..\r\n###...###...###...###...###...###...###.\r\n####....####....####....####....####....\r\n#####.....#####.....#####.....#####.....\r\n######......######......######......####\r\n#######.......#######.......#######.....", 
+                screen);
+            #endregion
+        }
+
+        [TestMethod]
+        public void RenderInput()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem10input.txt");
+            List<string[]> list = new List<string[]>();
+
+            foreach (string s in strings)
+            {
+                if (s != string.Empty)
+                {
+                    string[] line = s.Split(' ');
+                    list.Add(line);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Problem10 problem10 = new Problem10();
+            #endregion
+
+            #region Act
+            var screen = problem10.Render(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual("####.###...##..###..####.###...##....##.\r\n#....#..#.#..#.#..#.#....#..#.#..#....#.\r\n###..#..#.#....#..#.###..#..#.#.......#.\r\n#....###..#....###..#....###..#.......#.\r\n#....#.#..#..#.#.#..#....#....#..#.#..#.\r\n####.#..#..##..#..#.####.#.....##...##..", 
+                screen);
+            #endregion
+        }
+    }
+}
---
+++ AdventOfCode2022/Problem10.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Problem10
+    {
+        private HashSet<int> cycles = new HashSet<int>() { 20, 60, 100, 140, 180, 220 };
+        private HashSet<int> render = new HashSet<int>() { 40, 80, 120, 160, 200, 240 };
+
+        public int SignalStrength(List<string[]> list)
+        {
+            int signalStrength = 0;
+            int cycle = 1;
+            int x = 1;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var line = list[i];
+                if (cycles.Contains(cycle))
+                {
+                    signalStrength += cycle*x;
+                }
+                
+                if (list[i][0] == "addx")
+                {
+                    cycle++;
+
+                    if (cycles.Contains(cycle))
+                    {
+                        signalStrength += cycle*x;
+                    }
+
+                    x += Convert.ToInt32(list[i][1]);
+                }
+
+                cycle++;
+            }
+
+            while (cycle < 221)
+            {
+                if (cycles.Contains(cycle))
+                {
+                    signalStrength += cycle*x;
+                }
+                cycle++;
+            }
+
+
+            return signalStrength;
+        }
+
+        public string Render(List<string[]> list)
+        {
+            char[][] screen = new char[6][];
+            int cycle = 0;
+            int sprite = 1;
+            int pos = 0;
+            int v = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                screen[i] = new char[40];
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var line = list[i];
+
+                if (render.Contains(cycle))
+                {
+                    v++;
+                    pos = 0;
+                }
+
+                if (v < 6 && pos >= 0 && pos < 40 && pos >= sprite - 1 && pos <= sprite + 1)
+                {
+                    screen[v][pos] = '#';
+                }
+                pos++;
+
+                if (list[i][0] == "addx")
+                {
+                    cycle++;
+
+                    if (render.Contains(cycle))
+                    {
+                        v++;
+                        pos = 0;
+                    }
+
+                    if (v < 6 && pos >= 0 && pos < 40 && pos >= sprite - 1 && pos <= sprite + 1)
+                    {
+                        screen[v][pos] = '#';
+                    }
+                    pos++;
+
+                    sprite += Convert.ToInt32(list[i][1]);
+                }
+
+                cycle++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < 6; i++)
+            {
+                StringBuilder l = new StringBuilder();
+                for(int j = 0; j < 40; j++)
+                {
+                    if (screen[i][j] == '#')
+                    {
+                        l.Append('#');
+                    }
+                    else
+                    {
+                        l.Append('.');
+                    }    
+                }
+                sb.Append(l);
+
+                if (i != 5)
+                {
+                    sb.Append("\r\n");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
