2f6495fa59c42e873c50ec00aad69b22674cc5b8
Eric Ung <ungericw@tutanota.com>
2022-12-11 10:58:12 -0600

    Problem 11 messy parsing

---
+++ AdventOfCode2022.Tests/Problem11Tests.cs
@@ -0,0 +1,270 @@
+﻿namespace AdventOfCode2022.Tests
+{
+    [TestClass]
+    public class Problem11Tests
+    {
+        [TestMethod]
+        public void FindMostActiveTest1()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem11test1.txt");
+            List<IList<IList<string>>> list = new List<IList<IList<string>>>();
+
+            for(int i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                if (s != string.Empty)
+                {
+                    string[] line = s.Split(' ');
+                    List<IList<string>> monkey = new List<IList<string>>();
+                    if (line[0] == "Monkey")
+                    {
+                        List<string> id = new List<string> { line[1].Replace(":", "") };
+
+                        var itemsReplace = strings[++i].Replace(",", "");
+                        string[] itemsStr = itemsReplace.Trim().Split(' ');
+                        List<string> items = new List<string>();
+                        for (int j = 2; j < itemsStr.Length; j++)
+                        {
+                            items.Add(itemsStr[j]);
+                        }
+
+                        var operationStr = strings[++i].Trim().Split(' ');
+                        List<string> operation = new List<string>();
+                        for (int j = 3; j < operationStr.Length; j++)
+                        {
+                            operation.Add(operationStr[j]);
+                        }
+
+                        var testStr = strings[++i].Trim().Split(' ');
+                        List<string> test = new List<string>();
+                        for(int j = 1; j < testStr.Length; j++)
+                        {
+                            test.Add(testStr[j]);
+                        }
+
+                        string trueStr = strings[++i].Trim().Split(' ')[5];
+                        string falseStr = strings[++i].Trim().Split(' ')[5];
+                        List<string> condition = new List<string> { trueStr, falseStr };
+
+                        monkey.Add(id); // 0
+                        monkey.Add(items); // 1
+                        monkey.Add(operation); // 2
+                        monkey.Add(test); // 3
+                        monkey.Add(condition); // 4
+                    }
+                    list.Add(monkey);
+                }
+            }
+
+            Problem11 problem11 = new Problem11();
+            #endregion
+
+            #region Act
+            var mostActive = problem11.FindMostActive(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(10605, mostActive);
+            #endregion
+        }
+
+        [TestMethod]
+        public void FindMostActiveInput()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem11input.txt");
+            List<IList<IList<string>>> list = new List<IList<IList<string>>>();
+
+            for(int i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                if (s != string.Empty)
+                {
+                    string[] line = s.Split(' ');
+                    List<IList<string>> monkey = new List<IList<string>>();
+                    if (line[0] == "Monkey")
+                    {
+                        List<string> id = new List<string> { line[1].Replace(":", "") };
+
+                        var itemsReplace = strings[++i].Replace(",", "");
+                        string[] itemsStr = itemsReplace.Trim().Split(' ');
+                        List<string> items = new List<string>();
+                        for (int j = 2; j < itemsStr.Length; j++)
+                        {
+                            items.Add(itemsStr[j]);
+                        }
+
+                        var operationStr = strings[++i].Trim().Split(' ');
+                        List<string> operation = new List<string>();
+                        for (int j = 3; j < operationStr.Length; j++)
+                        {
+                            operation.Add(operationStr[j]);
+                        }
+
+                        var testStr = strings[++i].Trim().Split(' ');
+                        List<string> test = new List<string>();
+                        for(int j = 1; j < testStr.Length; j++)
+                        {
+                            test.Add(testStr[j]);
+                        }
+
+                        string trueStr = strings[++i].Trim().Split(' ')[5];
+                        string falseStr = strings[++i].Trim().Split(' ')[5];
+                        List<string> condition = new List<string> { trueStr, falseStr };
+
+                        monkey.Add(id); // 0
+                        monkey.Add(items); // 1
+                        monkey.Add(operation); // 2
+                        monkey.Add(test); // 3
+                        monkey.Add(condition); // 4
+                    }
+                    list.Add(monkey);
+                }
+            }
+
+            Problem11 problem11 = new Problem11();
+            #endregion
+
+            #region Act
+            var mostActive = problem11.FindMostActive(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(50616, mostActive);
+            #endregion
+        }
+
+        [TestMethod]
+        public void FindMostActiveLongTest1()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem11test1.txt");
+            List<IList<IList<string>>> list = new List<IList<IList<string>>>();
+
+            for(int i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                if (s != string.Empty)
+                {
+                    string[] line = s.Split(' ');
+                    List<IList<string>> monkey = new List<IList<string>>();
+                    if (line[0] == "Monkey")
+                    {
+                        List<string> id = new List<string> { line[1].Replace(":", "") };
+
+                        var itemsReplace = strings[++i].Replace(",", "");
+                        string[] itemsStr = itemsReplace.Trim().Split(' ');
+                        List<string> items = new List<string>();
+                        for (int j = 2; j < itemsStr.Length; j++)
+                        {
+                            items.Add(itemsStr[j]);
+                        }
+
+                        var operationStr = strings[++i].Trim().Split(' ');
+                        List<string> operation = new List<string>();
+                        for (int j = 3; j < operationStr.Length; j++)
+                        {
+                            operation.Add(operationStr[j]);
+                        }
+
+                        var testStr = strings[++i].Trim().Split(' ');
+                        List<string> test = new List<string>();
+                        for(int j = 1; j < testStr.Length; j++)
+                        {
+                            test.Add(testStr[j]);
+                        }
+
+                        string trueStr = strings[++i].Trim().Split(' ')[5];
+                        string falseStr = strings[++i].Trim().Split(' ')[5];
+                        List<string> condition = new List<string> { trueStr, falseStr };
+
+                        monkey.Add(id); // 0
+                        monkey.Add(items); // 1
+                        monkey.Add(operation); // 2
+                        monkey.Add(test); // 3
+                        monkey.Add(condition); // 4
+                    }
+                    list.Add(monkey);
+                }
+            }
+
+            Problem11 problem11 = new Problem11();
+            #endregion
+
+            #region Act
+            var mostActive = problem11.FindMostActiveLong(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(2713310158, mostActive);
+            #endregion
+        }
+
+        [TestMethod]
+        public void FindMostActiveLongInput()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem11input.txt");
+            List<IList<IList<string>>> list = new List<IList<IList<string>>>();
+
+            for(int i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                if (s != string.Empty)
+                {
+                    string[] line = s.Split(' ');
+                    List<IList<string>> monkey = new List<IList<string>>();
+                    if (line[0] == "Monkey")
+                    {
+                        List<string> id = new List<string> { line[1].Replace(":", "") };
+
+                        var itemsReplace = strings[++i].Replace(",", "");
+                        string[] itemsStr = itemsReplace.Trim().Split(' ');
+                        List<string> items = new List<string>();
+                        for (int j = 2; j < itemsStr.Length; j++)
+                        {
+                            items.Add(itemsStr[j]);
+                        }
+
+                        var operationStr = strings[++i].Trim().Split(' ');
+                        List<string> operation = new List<string>();
+                        for (int j = 3; j < operationStr.Length; j++)
+                        {
+                            operation.Add(operationStr[j]);
+                        }
+
+                        var testStr = strings[++i].Trim().Split(' ');
+                        List<string> test = new List<string>();
+                        for(int j = 1; j < testStr.Length; j++)
+                        {
+                            test.Add(testStr[j]);
+                        }
+
+                        string trueStr = strings[++i].Trim().Split(' ')[5];
+                        string falseStr = strings[++i].Trim().Split(' ')[5];
+                        List<string> condition = new List<string> { trueStr, falseStr };
+
+                        monkey.Add(id); // 0
+                        monkey.Add(items); // 1
+                        monkey.Add(operation); // 2
+                        monkey.Add(test); // 3
+                        monkey.Add(condition); // 4
+                    }
+                    list.Add(monkey);
+                }
+            }
+
+            Problem11 problem11 = new Problem11();
+            #endregion
+
+            #region Act
+            var mostActive = problem11.FindMostActiveLong(list);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(11309046332, mostActive);
+            #endregion
+        }
+    }
+}
---
+++ AdventOfCode2022/Problem11.cs
@@ -0,0 +1,157 @@
+﻿namespace AdventOfCode2022
+{
+    public class Problem11
+    {
+        long MOD;
+
+        public int FindMostActive(IList<IList<IList<string>>> monkeys)
+        {
+            int n = monkeys.Count;
+            int[] operations = new int[n];
+            Queue<int>[] monkeyItemsQueue = new Queue<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                monkeyItemsQueue[i] = new Queue<int>();
+                foreach(var s in monkeys[i][1])
+                {
+                    monkeyItemsQueue[i].Enqueue(Convert.ToInt32(s));
+                }
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int m = 0; m < n; m++)
+                {
+                    while (monkeyItemsQueue[m].Count > 0)
+                    {
+                        int currentItem = monkeyItemsQueue[m].Dequeue();
+                        currentItem = PerformOperation(currentItem, monkeys[m][2][1], monkeys[m][2][2]);
+                        currentItem /= 3;
+                        if (PerformDivisibilityTest(currentItem, Convert.ToInt32(monkeys[m][3][2])))
+                        {
+                            var throwTo = Convert.ToInt32(monkeys[m][4][0]);
+                            monkeyItemsQueue[throwTo].Enqueue(currentItem);
+                        }
+                        else
+                        {
+                            var throwTo = Convert.ToInt32(monkeys[m][4][1]);
+                            monkeyItemsQueue[throwTo].Enqueue(currentItem);
+                        }
+
+                        operations[m]++;
+                    }
+                }
+            }
+
+            Array.Sort(operations);
+            int mostActive = 0;
+            mostActive = operations[n - 1] * operations[n - 2];
+            return mostActive;
+        }
+
+        public long FindMostActiveLong(IList<IList<IList<string>>> monkeys)
+        {
+            int n = monkeys.Count;
+            int[] operations = new int[n];
+            Queue<long>[] monkeyItemsQueue = new Queue<long>[n];
+            MOD = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                monkeyItemsQueue[i] = new Queue<long>();
+                foreach(var s in monkeys[i][1])
+                {
+                    monkeyItemsQueue[i].Enqueue(long.Parse(s));
+                }
+
+                MOD *= long.Parse(monkeys[i][3][2]);
+            }
+
+            for (int i = 0; i < 10000; i++)
+            {
+                for (int m = 0; m < n; m++)
+                {
+                    while (monkeyItemsQueue[m].Count > 0)
+                    {
+                        long currentItem = monkeyItemsQueue[m].Dequeue();
+                        currentItem = PerformOperationLong(currentItem, monkeys[m][2][1], monkeys[m][2][2]);
+                        currentItem = currentItem % MOD;
+                        if (PerformDivisibilityTestLong(currentItem, Convert.ToInt32(monkeys[m][3][2])))
+                        {
+                            var throwTo = Convert.ToInt32(monkeys[m][4][0]);
+                            monkeyItemsQueue[throwTo].Enqueue(currentItem);
+                        }
+                        else
+                        {
+                            var throwTo = Convert.ToInt32(monkeys[m][4][1]);
+                            monkeyItemsQueue[throwTo].Enqueue(currentItem);
+                        }
+
+                        operations[m]++;
+                    }
+                }
+            }
+
+            Array.Sort(operations);
+            long mostActive = 0;
+            mostActive = (long)operations[n - 1] * (long)operations[n - 2];
+            return mostActive;
+        }
+
+        private long PerformOperationLong(long value, string operation, string num)
+        {
+            long opNum = 0;
+            if (num == "old")
+            {
+                opNum = value;
+            }
+            else
+            {
+                opNum = long.Parse(num);
+            }
+
+            switch(operation)
+            {
+                case "+":
+                    return value + opNum;
+                case "*":
+                    return value * opNum;
+            }
+
+            return 0;
+        }
+
+        private bool PerformDivisibilityTestLong(long value, long divisor)
+        {
+            return value % divisor == 0;
+        }
+
+        private int PerformOperation(int value, string operation, string num)
+        {
+            int opNum = 0;
+            if (num == "old")
+            {
+                opNum = value;
+            }
+            else
+            {
+                opNum = Convert.ToInt32(num);
+            }
+
+            switch(operation)
+            {
+                case "+":
+                    return value + opNum;
+                case "*":
+                    return value * opNum;
+            }
+
+            return 0;
+        }
+
+        private bool PerformDivisibilityTest(int value, int divisor)
+        {
+            return value % divisor == 0;
+        }
+    }
+}
