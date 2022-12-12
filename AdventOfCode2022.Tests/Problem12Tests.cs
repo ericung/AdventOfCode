821e60a5ac33526faa1396b77073237c8025c7f7
Eric Ung <ungericw@tutanota.com>
2022-12-12 09:40:54 -0600

    Problem 12

---
+++ AdventOfCode2022.Tests/Problem12Tests.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestClass]
+    public class Problem12Tests
+    {
+        [TestMethod]
+        public void TraverseTest1()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem12test1.txt");
+            List<IList<char>> list = new List<IList<char>>();
+            int startR = -1;
+            int startC = -1;
+            int endR = -1;
+            int endC = -1;
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                if (s != string.Empty)
+                {
+                    List<char> row = new List<char>();
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        var c = s[j];
+                        if (s[j] == 'S')
+                        {
+                            startR = i;
+                            startC = j;
+                            c = 'a';
+                        }
+                        else if (s[j] == 'E')
+                        {
+                            endR = i;
+                            endC = j;
+                            c = 'z';
+                        }
+
+                        row.Add(c);
+                    }
+                    list.Add(row);
+                }
+            }
+
+            int m = list.Count;
+            int n = list[0].Count;
+
+            char[,] map = new char[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    map[i, j] = list[i][j];
+                }
+            }
+
+            Problem12 problem12 = new Problem12();
+            #endregion
+
+            #region Act
+            var mostActive = problem12.Traverse(map, startR, startC, endR, endC);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(31, mostActive);
+            #endregion
+        }
+        
+        [TestMethod]
+        public void TraverseInput()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem12input.txt");
+            List<IList<char>> list = new List<IList<char>>();
+            int startR = -1;
+            int startC = -1;
+            int endR = -1;
+            int endC = -1;
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                if (s != string.Empty)
+                {
+                    List<char> row = new List<char>();
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        var c = s[j];
+                        if (s[j] == 'S')
+                        {
+                            startR = i;
+                            startC = j;
+                            c = 'a';
+                        }
+                        else if (s[j] == 'E')
+                        {
+                            endR = i;
+                            endC = j;
+                            c = 'z';
+                        }
+
+                        row.Add(c);
+                    }
+                    list.Add(row);
+                }
+            }
+
+            int m = list.Count;
+            int n = list[0].Count;
+
+            char[,] map = new char[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    map[i, j] = list[i][j];
+                }
+            }
+
+            Problem12 problem12 = new Problem12();
+            #endregion
+
+            #region Act
+            var mostActive = problem12.Traverse(map, startR, startC, endR, endC);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(423, mostActive);
+            #endregion
+        }
+
+        [TestMethod]
+        public void TraverseMultipleTest1()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem12test1.txt");
+            List<IList<char>> list = new List<IList<char>>();
+            int startR = -1;
+            int startC = -1;
+            int endR = -1;
+            int endC = -1;
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                if (s != string.Empty)
+                {
+                    List<char> row = new List<char>();
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        var c = s[j];
+                        if (s[j] == 'S')
+                        {
+                            startR = i;
+                            startC = j;
+                            c = 'a';
+                        }
+                        else if (s[j] == 'E')
+                        {
+                            endR = i;
+                            endC = j;
+                            c = 'z';
+                        }
+
+                        row.Add(c);
+                    }
+                    list.Add(row);
+                }
+            }
+
+            int m = list.Count;
+            int n = list[0].Count;
+
+            char[,] map = new char[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    map[i, j] = list[i][j];
+                }
+            }
+
+            Problem12 problem12 = new Problem12();
+            #endregion
+
+            #region Act
+            var mostActive = problem12.TraverseMultiple(map, endR, endC);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(29, mostActive);
+            #endregion
+        }
+
+        [TestMethod]
+        public void TraverseMultipleInput()
+        {
+            #region Arrange
+            string[] strings = Helper.GetText("problem12input.txt");
+            List<IList<char>> list = new List<IList<char>>();
+            int startR = -1;
+            int startC = -1;
+            int endR = -1;
+            int endC = -1;
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                var s = strings[i];
+                if (s != string.Empty)
+                {
+                    List<char> row = new List<char>();
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        var c = s[j];
+                        if (s[j] == 'S')
+                        {
+                            startR = i;
+                            startC = j;
+                            c = 'a';
+                        }
+                        else if (s[j] == 'E')
+                        {
+                            endR = i;
+                            endC = j;
+                            c = 'z';
+                        }
+
+                        row.Add(c);
+                    }
+                    list.Add(row);
+                }
+            }
+
+            int m = list.Count;
+            int n = list[0].Count;
+
+            char[,] map = new char[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    map[i, j] = list[i][j];
+                }
+            }
+
+            Problem12 problem12 = new Problem12();
+            #endregion
+
+            #region Act
+            var mostActive = problem12.TraverseMultiple(map, endR, endC);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(416, mostActive);
+            #endregion
+        }
+    }
+}
---
+++ AdventOfCode2022/Problem12.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Problem12
+    {
+        private int[,] directions = new int[4,2] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+        public int Traverse(char[,] map, int startR, int startC, int endR, int endC)
+        {
+            int m = map.GetLength(0);
+            int n = map.GetLength(1);
+            int steps = 0;
+            Queue<(int, int)> queue = new Queue<(int, int)>();
+            queue.Enqueue((startR, startC));
+            HashSet<(int,int)> visited = new HashSet<(int, int)> ();
+            visited.Add((startR, startC));
+
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    (int r, int c) = queue.Dequeue();
+
+                    if (r == endR && c == endC)
+                    {
+                        return steps;
+                    }
+
+                    for (int d = 0; d < directions.GetLength(0); d++)
+                    {
+                        int newR = r + directions[d, 0];
+                        int newC = c + directions[d, 1];
+
+                        if ((newR >= 0 && newR < m && newC >= 0 && newC < n) && !visited.Contains((newR, newC)))
+                        {
+                            if (map[newR, newC] - map[r, c] <= 1)
+                            {
+                                visited.Add((newR, newC));
+                                queue.Enqueue((newR, newC));
+                            }
+                        }
+                    }
+                }
+
+                steps++;
+            }
+            
+            return 0;
+        }
+
+        public int TraverseMultiple(char[,] map, int endR, int endC)
+        {
+            int m = map.GetLength(0);
+            int n = map.GetLength(1);
+            int steps = 0;
+            Queue<(int, int)> queue = new Queue<(int, int)>();
+            HashSet<(int,int)> visited = new HashSet<(int, int)> ();
+
+            for(int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (map[i,j] == 'a')
+                    {
+                        queue.Enqueue((i, j));
+                        visited.Add((i, j));
+                    }
+                }
+            }
+
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    (int r, int c) = queue.Dequeue();
+
+                    if (r == endR && c == endC)
+                    {
+                        return steps;
+                    }
+
+                    for (int d = 0; d < directions.GetLength(0); d++)
+                    {
+                        int newR = r + directions[d, 0];
+                        int newC = c + directions[d, 1];
+
+                        if ((newR >= 0 && newR < m && newC >= 0 && newC < n) && !visited.Contains((newR, newC)))
+                        {
+                            if (map[newR, newC] - map[r, c] <= 1)
+                            {
+                                visited.Add((newR, newC));
+                                queue.Enqueue((newR, newC));
+                            }
+                        }
+                    }
+                }
+
+                steps++;
+            }
+            
+            return 0;
+        }
+    }
+}
